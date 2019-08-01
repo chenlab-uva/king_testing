ba9808052017ed2b63e966941fa6e7949e009297
Maria Murach <mm5jy@virginia.edu>
2019-08-01 09:31:10 -0400

    Changes in parameters.

--- test.py
+++ test.py
@@ -15,38 +15,30 @@ import shutil
 cur_dir = os.path.dirname(os.path.realpath(__file__))
 king_path = os.path.join(cur_dir, "king_src")
 data = os.path.join(cur_dir, "data")
-bed = ""
-king_exe = ""
-
+bed = "ex.bed"
+king_exe = os.path.join(king_path, "king")
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-l', dest='local', action="store_true",
-                    default=False, help="Use local KING installed in this system.")
 parser.add_argument('-c', dest='clean', action="store_true",
                     default=False, help="Clean directory from previous testing.")
-parser.add_argument('-d', dest='example_data', action="store",
-                    default="", help="Path to example data.")
+
 
 def prepare_tested_data():
     print("Preparing tests data ...")
     url = "http://people.virginia.edu/~wc9c/KING/ex.tar.gz"
     handle_tarball(url, "data")
     global data
-    data = os.path.join(cur_dir, "data")
     global bed
     bed = os.path.join(data, "ex.bed")
-    print("Preparinge tests data finished.")
+    print("Preparing tests data finished.")
 
 
 def prepare_king_source():
     print("Preparing KING's source code ...")
     url = "http://people.virginia.edu/~wc9c/KING/KINGcode.tar.gz"
     handle_tarball(url, "king_src")
-    king_obj = os.path.join(cur_dir, "king_src", "*.cpp")
-    global king_exe
-    king_exe = os.path.join(king_path, "king")
-    command = ["c++", "-lm", "-O2", "-fopenmp", "-o",
-               "{}".format(king_exe), "{}".format(king_obj), "-lz"]
+    king_obj = os.path.join(king_path, "*.cpp")
+    #command = ["c++", "-lm", "-O2", "-fopenmp", "-o", "{}".format(king_exe), "{}".format(king_obj), "-lz"]
     #subprocess.run(command, shell=True)
     os.system("c++ -lm -O2 -fopenmp -o {} {} -lz".format(king_exe, king_obj))
     print("Preparing KING's source code finished.")
@@ -75,27 +67,27 @@ def clean_repository():
 def handle_kings_output(out):
     out = out.decode()
     lines = out.split("\n")
-    #print(lines)
+    # print(lines)
     summary = []
-    inside_summary = False 
+    inside_summary = False
     for line in lines:
-        if line.startswith("Relationship summary") or inside_summary:
-            inside_summary = True
+        if inside_summary: 
             summary.append(line)
-    print(" ".join(summary))
+        if line.startswith("Autosome genotypes") or inside_summary:
+            inside_summary = True
+        
+    print("\n".join(summary))
 
 
 class KingTestCase(unittest.TestCase):
-    def format_command(self, param):
-        global king_exe, bed
-        command = ["{}".format(king_exe), "-b",
-                   "{}".format(bed), "{}".format(param)]
-        print(command)
-        return command
+   #def setUp(self):
+    #    prepare_tested_data()
+     #   prepare_king_source()
 
-    def setUp(self):
-        prepare_tested_data()
-        prepare_king_source()
+    def format_command(self, param):
+        command = ["{}".format(king_exe), "-b",
+                   "{}".format(bed), "--prefix", "{}".format(king_path + "/"), "{}".format(param)]
+        return command
 
     def test_default_widget_size(self):
         self.assertEqual((50, 50), (50, 50), 'incorrect default size')
@@ -109,8 +101,10 @@ class KingTestCase(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    options = parser.parse_args()
+    options = parser.parse_known_args()[0]
     if options.clean:
         clean_repository()
         sys.exit()
+    prepare_tested_data()
+    prepare_king_source()
     unittest.main()
